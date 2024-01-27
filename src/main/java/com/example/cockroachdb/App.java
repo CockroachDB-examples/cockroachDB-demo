22bf6929f2c9a877ec715dda4c0ca649dc4be914
Pippa Newbold <pippa.newbold@gmail.com>
2024-01-27 08:50:13 +0000

    application properties

--- src/main/java/com/example/cockroachdb/App.java
+++ src/main/java/com/example/cockroachdb/App.java
@@ -7,48 +7,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-public class App {
+@SpringBootApplication
+public class App implements CommandLineRunner {
 
-    private static final String JDBC_DATABASE_URL = "jdbc:postgresql://%s:%s/%s?sslmode=verify-full&password=%s&user=%s";
+    public static final String QUERY = "SELECT message FROM messages";
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    public App() {}
+
+    App(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getResults());
+        SpringApplication.run(App.class, args);
     }
 
     public List<String> getResults() {
-        try {
-            PGSimpleDataSource ds = new PGSimpleDataSource();
-            ds.setApplicationName("docs_quickstart_java");
-            ds.setUrl(String.format(JDBC_DATABASE_URL,
-                    System.getProperty("cockroachDBCluster"),
-                    System.getProperty("cockroachDBPort"),
-                    System.getProperty("cockroachDBDatabase"),
-                    System.getProperty("cockroachDBPassword"),
-                    System.getProperty("cockroachDBUser")));
-            Connection connection = ds.getConnection();
-            return executeStmt(connection);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return jdbcTemplate.query(QUERY, (rs, rowNum) -> rs.getString("message"));
     }
 
-    private List<String> executeStmt(final Connection conn) {
-        try {
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("SELECT message FROM messages");
-            final List<String> results = new ArrayList<>();
-
-            while (rs.next()) {
-                results.add(rs.getString(1));
-            }
-            rs.close();
-            st.close();
-            return results;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    @Override
+    public void run(String... args) {
+        System.out.println(getResults().getFirst());
     }
 }
--- src/test/java/com/example/cockroachdb/AppTest.java
+++ src/test/java/com/example/cockroachdb/AppTest.java
@@ -1,16 +1,23 @@
 package com.example.cockroachdb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 public class AppTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     public void canGetMessagesFromDatabase() {
-        final List<String> results = new App().getResults();
+        final List<String> results = new App(jdbcTemplate).getResults();
         assertEquals(1, results.size());
         assertEquals("Successfully Created Table, created a row and read it back!", results.getFirst());
     }
